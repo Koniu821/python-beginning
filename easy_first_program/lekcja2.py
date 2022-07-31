55ed4f96e317d6dcffd0d21eb5b2a2d308f9d07e
Rafal Konik <rafal.konik12@gmail.com>
2022-07-31 11:04:52 +0200

    lekcja2

---
+++ easy_first_program/lekcja2.py
@@ -0,0 +1,34 @@
+#x = True #zawsze dużą literą True False.
+#y = False  # Te zmienne można  tylko i wyłacznie przechowyać w zmiennej typu logicznego
+
+#print(x)
+#print(y)
+
+#rint(5==5) # operator sprawdza czy jedna strona równa się drugiej 
+
+#print(5!=1) # sprawdzenie czy lewa strona jest inna niż prawa 
+#print(5>5) #czy lewa jest większa od prawej
+#print(5<5) #czy lewa jest mniejsza od prawej
+#print(7<=7) # czy lewa jest mniejsza lub równa prawej
+#print(7>=7) # czy lewaj jest większa lub równa prawej
+
+
+#if 5 == 5: #Warunek if kończymy : i nie dajemy ()
+    #print("Prawda") #Instrukcje do if zapisujemy po wcięciu (Tab)
+#print("Koniec")    
+#if 15>10:
+    #print("Większe")
+#elif - polecenie dzieki któremu można dodać kolejny warunek jeśli warunek if nie został spełniony.    
+#else:#Jeżel warunek if jest niespełniony
+    #print("Mniejsze")     
+    
+x = 10
+y = 10
+if x>y:
+    print("x większe Y")    
+elif x < y:
+    print("X mniejsze Y")
+elif x == 10:
+    print("X równe 10")
+else:
+    print("X równe Y")    
