8e165836fa66bd93a944b0db38a4d7d15dd9f541
Rafal Konik <rafal.konik12@gmail.com>
2022-07-31 11:37:50 +0200

    lekcja3

---
+++ easy_first_program/lekcja3.py
@@ -0,0 +1,19 @@
+#wiek = 12
+#kasa = 40
+
+
+
+#if wiek >= 18 and kasa >=35:  #Dwa warunki w jednym if
+ #   print("Może wejść")
+#if wiek <= 12 or kasa >= 30: #Jeżeli pierwszy warunek nie został spełniony przechodzi do drugiego
+ #    print("Może wejść")
+#if not wiek > 12  or kasa >=30:
+  #  print("Możesz wejść")
+#else:
+  #  print("Nie może wejść")
+
+
+if True or False and False: #kolejność operatorów najpier wykona się and następnie or , można zastosowac nawiasy
+    print("Prawda")
+else:
+    print("Fałsz")
